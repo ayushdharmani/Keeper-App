bc0e5ee407892ed191f3db23e83150f389517cc1
digibros <ayushdharmani02@gmail.com>
2022-08-24 01:30:53 +0530

    Updates

---
+++ precache-manifest.9879d5664670c43884da9481a7685275.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f20615ed0a8d1d9c62e354a28f53188",
+    "url": "/Keeper-App/index.html"
+  },
+  {
+    "revision": "5a375caa7684772f30bf",
+    "url": "/Keeper-App/static/js/2.906c33bb.chunk.js"
+  },
+  {
+    "revision": "740243c43f30469d6ead",
+    "url": "/Keeper-App/static/js/main.9be1a577.chunk.js"
+  },
+  {
+    "revision": "8ac763be5021583d99b9",
+    "url": "/Keeper-App/static/js/runtime-main.7fdd9867.js"
+  }
+]);
