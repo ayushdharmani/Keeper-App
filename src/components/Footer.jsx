dac5c65fb900d05f87bce75989df7e9dabeabcc9
digibros <ayushdharmani02@gmail.com>
2022-08-24 00:56:31 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+  const year = new Date().getFullYear();
+  return (
+    <footer>
+      <p>Developed and Design by <em>Ayush Dharmani</em></p>
+    </footer>
+  );
+}
+
+export default Footer;
